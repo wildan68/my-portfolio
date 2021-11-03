ae162d8c256a847f323bb7ccbc0e500deebf7564
Mohammad Wildan Rizkiyadi <30789940+wildan68@users.noreply.github.com>
2021-11-03 13:00:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'header.html';
+include 'svg-icon-hover.html';
+include 'home.html';
+include 'about.html';
+include 'skills.html';
+include 'qualification.html';
+include 'service.html';
+include 'portofolio.html';
+include 'contact.html';
+include 'footer.html';
+
+?>
